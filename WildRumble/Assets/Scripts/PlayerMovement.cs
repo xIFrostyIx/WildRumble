c26aa40de66dad99d408acc4bde3e71e954dbdc3
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-09-17 23:34:29 -0500

    Tweak First person camera

--- WildRumble/Assets/Scripts/PlayerMovement.cs
+++ WildRumble/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Created by Joshua Guerrero
+ * This script sets up first person movement
+ * by allowing values to be adjusted in
+ * the inspector
+ */
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
+    //set to 6
     public float moveSpeed;
+    //set to 5
+    public float groundDrag;
+
+   
+    [Header("Ground Check")]
+    //set to 2
+    public float playerHeight;
+    //Set as Ground
+    public LayerMask Ground;
+    bool grounded;
+
 
     public Transform orientation;
 
@@ -18,13 +36,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        //so the player model doesn't fall over
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
     private void Update()
     {
+        //checks if there is ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+
         MyInput();
+        SpeedControl();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
     private void FixedUpdate()
@@ -40,8 +68,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
+        //calculate the player's moevement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
