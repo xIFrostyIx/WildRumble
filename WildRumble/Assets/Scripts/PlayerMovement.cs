c951d0b831bc17d50ea5d3fbc4c4b1b306fae554
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-09-19 12:24:44 -0500

    Updated player movement

--- WildRumble/Assets/Scripts/PlayerMovement.cs
+++ WildRumble/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Created by Joshua Guerrero
+ * This script sets up first person movement
+ * by allowing values to be adjusted in
+ * the inspector
+ */
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
+    //set to 6
     public float moveSpeed;
+    //set to 5
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+   
+    [Header("Ground Check")]
+    //set to 2
+    public float playerHeight;
+    //Set as Ground
+    public LayerMask Ground;
+    bool grounded;
+
 
     public Transform orientation;
 
@@ -18,13 +43,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        //so the player model doesn't fall over
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
     private void Update()
     {
+        //checks if there is ground
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+
         MyInput();
+        SpeedControl();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
     private void FixedUpdate()
@@ -36,12 +71,57 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
+        //calculate the player's moevement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        // when on the ground
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // when in the air
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset Y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
