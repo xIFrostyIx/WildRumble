79aa13f0f52f659e78c879326db3b5e81aad5f18
AzureWolfe <utsawolfe@yahoo.com>
2024-09-23 14:06:18 -0500

    Lvl 2 adjustments
    added flashlight
    Testing lights

---
+++ WildRumble/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light ThisLight;
+    public bool LightToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisLight = GetComponent<Light>();
+        ThisLight.intensity = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (ThisLight.intensity == 0)
+            {
+                ThisLight.intensity = 1;
+                LightToggle = true;
+            }
+            else if (ThisLight.intensity == 1)
+            {
+                ThisLight.intensity = 0;
+                LightToggle = false;
+            }
+        }
+    }
+}
