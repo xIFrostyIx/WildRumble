d6b400659eab94e5b4db795ffc05ceb3b87f258d
darcy1009 <punnguyen18999@gmail.com>
2024-09-24 03:22:18 -0500

    Added loading scenes and BGM

--- WildRumble/Assets/Scripts/ButtonManager.cs
+++ WildRumble/Assets/Scripts/ButtonManager.cs
@@ -4,20 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject optionsMenuUI; // Reference to the options menu UI
-    public Slider volumeSlider; // Reference to the volume slider
+    public GameObject optionsMenuUI; 
+    public Slider volumeSlider; 
 
     void Start()
     {
         if (volumeSlider != null)
         {
-            // Load the saved volume setting
+            
             volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
             AudioListener.volume = volumeSlider.value;
 
-            // Add listener to the volume slider
+            
             volumeSlider.onValueChanged.AddListener(SetVolume);
-       
         }
     }
 
@@ -59,7 +58,13 @@ public class ButtonManager : MonoBehaviour
 
     public void SetVolume(float volume)
     {
-        AudioListener.volume = volume; // Adjust the global audio volume
-        PlayerPrefs.SetFloat("Volume", volume); // Save the volume setting
+        AudioListener.volume = volume; 
+        PlayerPrefs.SetFloat("Volume", volume); 
+    }
+
+    
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); 
     }
 }
