14a809cef1672a823dd1f7e19ce73f607115d1db
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-10-11 01:08:01 -0500

    Updated AI movement

--- WildRumble/Assets/Scripts/EnemyAI.cs
+++ WildRumble/Assets/Scripts/EnemyAI.cs
@@ -3,87 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/*
+ * Created by: Joshua Guerrero
+ * This script sets the behavior of
+ * AI enemies in the game.
+ * 
+ * NOTE: Stats can be changed based off enemy type
+ */
+
 public class EnemyAI : MonoBehaviour
 {
-    public NavMeshAgent navAgent;
+    public Transform player; // Assign the player object in the Inspector
+    public float chaseRange = 10f; // The range at which the AI will start chasing the player
+    public float stoppingDistance = 2f; // The distance at which the AI will stop chasing
+    public float wanderRange = 5f; // The range within which the AI will wander
+    public float wanderTimer = 2f; // Time interval for wandering
+    public float speed = 3.5f; //Enemies speed
 
-    public Transform player;
+    private NavMeshAgent navMeshAgent;
+    private float timer;
 
-    public LayerMask Ground, Player;
-
-    //Patrol Variables
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    //Attacking Variables
-    public float timeBetweenAttack;
-    bool alreadyAttacked;
-
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
+    void Start()
     {
-        player = GameObject.Find("PlayerObj").transform;
-        navAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = speed;// set initial speed
+        timer = wanderTimer; // Initialize the timer
     }
 
-    private void Update()
+    void Update()
     {
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
-    }
-
-    private void Patroling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            navAgent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-    }
-
-    private void SearchWalkPoint()
-    {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
-            walkPointSet = true;
-    }
-
-    private void ChasePlayer()
-    {
-        navAgent.SetDestination(player.position);
-    }
-
-    private void AttackPlayer()
-    {
-        navAgent.SetDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
+        // Check if the player is within chase range
+        if (distanceToPlayer < chaseRange)
         {
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttack);
+            // Move towards the player if outside the stopping distance
+            if (distanceToPlayer > stoppingDistance)
+            {
+                navMeshAgent.SetDestination(player.position);
+            }
+            else
+            {
+                navMeshAgent.SetDestination(transform.position); // Stop moving
+            }
+        }
+        else
+        {
+            Wander();
         }
     }
 
-    private void ResetAttack()
+    void Wander()
     {
-        alreadyAttacked = false; 
-    }
+        // Update the timer
+        timer += Time.deltaTime;
+        if (timer >= wanderTimer)
+        {
+            // Set a new random destination within wander range
+            Vector3 randomDirection = Random.insideUnitSphere * wanderRange;
+            randomDirection += transform.position; // Offset the random direction by the AI's position
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, wanderRange, NavMesh.AllAreas);
+            navMeshAgent.SetDestination(hit.position);
 
+            timer = 0; // Reset the timer
+        }
+    }
 }
