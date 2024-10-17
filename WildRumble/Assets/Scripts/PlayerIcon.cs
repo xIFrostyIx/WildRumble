2f83b5f364b63b1c43fe0dea45548a784d1e6372
Joshua Guerrero <62621784+xIFrostyIx@users.noreply.github.com>
2024-10-17 00:33:20 -0500

    Fixed Mini Map Player Icon

--- WildRumble/Assets/Scripts/PlayerIcon.cs
+++ WildRumble/Assets/Scripts/PlayerIcon.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Created By Joshua Guerrero
+ * Allows the player icon to rotate with the player
+ */
+
 public class PlayerIcon : MonoBehaviour
 {
-    public GameObject player; // Assign your player GameObject in the Inspector
-    public RectTransform miniMapIcon; // Assign your mini-map icon RectTransform in the Inspector
+    public Transform player; // Assign the player's transform in the inspector
+    public RectTransform icon; // Assign the mini map icon RectTransform in the inspector
 
     void Update()
     {
-        if (player != null && miniMapIcon != null)
-        {
-            // Get the player's rotation
-            Quaternion playerRotation = player.transform.rotation;
-
-            // Convert to Euler angles (using y-axis for 2D rotation)
-            float angle = playerRotation.eulerAngles.y;
-
-            // Rotate the mini-map icon
-            miniMapIcon.rotation = Quaternion.Euler(0, 0, angle);
-        }
+         // Rotate the icon to match the player's rotation
+        icon.localEulerAngles = new Vector3(0, 0, -player.eulerAngles.y); // Negative for correct orientation
     }
 }
