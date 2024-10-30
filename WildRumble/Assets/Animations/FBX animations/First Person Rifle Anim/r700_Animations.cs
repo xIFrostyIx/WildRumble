ae1fc83abc2e042065730bde0bd0e0aaa765a71d
Schmuckerton <ebenmtz09@gmail.com>
2024-10-30 02:05:12 -0500

    Rifle Animation Testing and Script

---
+++ WildRumble/Assets/Animations/FBX animations/First Person Rifle Anim/r700_Animations.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class r700_Animations : MonoBehaviour
+{
+
+    Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            myAnimator.SetBool("rifleADS", true);
+        }
+
+        if (!Input.GetButtonDown("Fire2"))
+        {
+            myAnimator.SetBool("rifleADS", false);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            myAnimator.SetBool("rifleShoot", true);
+        }
+
+        if (!Input.GetButtonDown("Fire1"))
+        {
+            myAnimator.SetBool("rifleShoot", false);
+        }
+
+    }
+}
