0595676dbfeed1afc951a651639a3024cf0fdc98
darcy1009 <punnguyen18999@gmail.com>
2024-11-05 01:28:34 -0600

    Fix gunshot, health bar, added losing music

--- WildRumble/Assets/Scripts/ButtonManager.cs
+++ WildRumble/Assets/Scripts/ButtonManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-//Made by Darcy
+
 public class ButtonManager : MonoBehaviour
 {
     public GameObject optionsMenuUI;
@@ -11,7 +11,7 @@ public class ButtonManager : MonoBehaviour
     public btnFX buttonFXScript;
     public AudioSource bgmAudioSource;
 
-    public HealthBar healthBar; 
+    public HealthBar healthBar;
 
     void Start()
     {
@@ -37,6 +37,12 @@ public class ButtonManager : MonoBehaviour
             gunShotSoundSlider.value = PlayerPrefs.GetFloat("GunShotVolume", 1f);
             gunShotSoundSlider.onValueChanged.AddListener(SetGunShotVolume);
         }
+
+        // Link the BGM audio source to HealthBar
+        if (healthBar != null)
+        {
+            healthBar.bgmAudioSource = bgmAudioSource;
+        }
     }
 
     public void SetButtonFXVolume(float volume)
@@ -59,7 +65,7 @@ public class ButtonManager : MonoBehaviour
         Weapon weaponScript = FindObjectOfType<Weapon>();
         if (weaponScript != null)
         {
-            weaponScript.gunShotVolume = volume;
+            weaponScript.gunShotVolume = volume; // Set the gunshot volume
             PlayerPrefs.SetFloat("GunShotVolume", volume);
         }
 
--- WildRumble/Assets/Scripts/Weapon.cs
+++ WildRumble/Assets/Scripts/Weapon.cs
@@ -15,9 +15,9 @@ public class Weapon : MonoBehaviour
     private bool isReloading = false;
 
     public AudioClip gunShotSound;
-    public AudioClip reloadSound; // New reload sound variable
+    public AudioClip reloadSound;
     private AudioSource audioSource;
-    public float gunShotVolume = 1f;
+    public float gunShotVolume = 1f; // Gunshot volume
 
     public FleeingEnemyManager fleeingEnemyManager;
 
@@ -26,11 +26,13 @@ public class Weapon : MonoBehaviour
         currentAmmo = maxAmmo;
 
         audioSource = GetComponent<AudioSource>();
-
         if (audioSource == null)
         {
             audioSource = gameObject.AddComponent<AudioSource>();
         }
+
+        // Load gunShotVolume from PlayerPrefs
+        gunShotVolume = PlayerPrefs.GetFloat("GunShotVolume", 1f);
     }
 
     void Update()
@@ -90,8 +92,7 @@ public class Weapon : MonoBehaviour
     {
         isReloading = true;
         Debug.Log("Reloading...");
-        // Edited by Darcy
-        PlayReloadSound(); 
+        PlayReloadSound();
 
         yield return new WaitForSeconds(reloadTime);
 
