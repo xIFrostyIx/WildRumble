cb60407ead68fc6909b8f83aed5bd6c8d344557b
AzureWolfe <utsawolfe@yahoo.com>
2024-12-07 00:20:32 -0600

    level adjustments, hunter body moved slightly back

--- WildRumble/Assets/Scripts/Ammo UI.cs
+++ WildRumble/Assets/Scripts/Ammo UI.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AmmoUI : MonoBehaviour
 {
+    public RaycastRifle weapon;
+    public TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateAmmoText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAmmoText();
+    }
+    
+    public void UpdateAmmoText() 
+    {
+        text.text = $"{weapon.CurrentMag} / {weapon.MaxMagSize}\n{weapon.CurrentAmmo} / {weapon.MaxAmmoSize}";
     }
 }
